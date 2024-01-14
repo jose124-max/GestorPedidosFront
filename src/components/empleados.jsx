609cfa38d8e2da97753d19f4e3d0078c02dfcbfc
lreyesp26 <lreyesp@uteq.edu.ec>
2024-01-14 13:14:33 -0500

    Sucursales en empleado agregadas

--- src/components/empleados.jsx
+++ src/components/empleados.jsx
@@ -1,36 +1,44 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { UserOutlined } from '@ant-design/icons';
-import { Divider, Avatar, Flex, Segmented, Tooltip,Drawer  } from 'antd';
+import { Divider, Avatar, Flex, Segmented, Tooltip, Select } from 'antd';
 import { Container, Row, Col } from 'react-bootstrap';
-import repartidor from './res/repartidor.png'
-import administrador from './res/administrador.png'
+import repartidor from './res/repartidor.png';
+import administrador from './res/administrador.png';
 import camarero from './res/camarero.png';
 import cocinero from './res/cocinero.png';
 import EditarEmpleado from './EditarEmpleado';
-import CrearEmpleadoForm from './crearempleado';
-import anadir from './res/anadir.png'
+import anadir from './res/anadir.png';
+
+const { Option } = Select;
 
 const Empleados = ({ }) => {
     const [selectedOficio, setSelectedOficio] = useState('Administradores');
-    const [opene, setOpene] = useState(false);
+    const [selectedSucursal, setSelectedSucursal] = useState(0);
+    const [sucursales, setSucursales] = useState([]);
+    const [loadingSucursales, setLoadingSucursales] = useState(true);
 
-    const showDrawere = () => {
-        setOpene(true);
-    };
-
-    const onClosee = () => {
-        setSelectedOficio('Administradores');
-        setOpene(false);
-    };
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/sucursal/sucusarleslist/')
+            .then((response) => response.json())
+            .then((data) => {
+                setSucursales(data.sucursales);  // Utilizar data.sucursales en lugar de data
+            })
+            
+            .catch((error) => {
+                console.error('Error fetching sucursales:', error);
+            })
+            .finally(() => {
+                setLoadingSucursales(false);
+            });
+    }, []);
 
     const handleOficioChange = (value) => {
-        if (value == 'agregar') {
-            showDrawere();
-            return;
-        }
         setSelectedOficio(value);
     };
 
+    const handleSucursalChange = (value) => {
+        setSelectedSucursal(value);
+    };
 
     return (
         <>
@@ -65,7 +73,7 @@ const Empleados = ({ }) => {
                                 label: (
                                     <Tooltip title="Meseros">
                                         <div style={{ padding: 4 }}>
-                                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} size="large" src={camarero}/>
+                                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} size="large" src={camarero} />
                                         </div>
                                     </Tooltip>
                                 ),
@@ -75,7 +83,8 @@ const Empleados = ({ }) => {
                                 label: (
                                     <Tooltip title="Jefes de cocina">
                                         <div style={{ padding: 4 }}>
-                                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} size="large" src={cocinero}/>                                        </div>
+                                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} size="large" src={cocinero} />
+                                        </div>
                                     </Tooltip>
                                 ),
                                 value: 'JefesCocina',
@@ -84,34 +93,35 @@ const Empleados = ({ }) => {
                                 label: (
                                     <Tooltip title="Agregar empleados">
                                         <div style={{ padding: 4 }}>
-                                            <Avatar style={{ backgroundColor: '#ffff' }} icon={<UserOutlined />} size="large" src={anadir}/>                                        </div>
+                                            <Avatar style={{ backgroundColor: '#ffff' }} icon={<UserOutlined />} size="large" src={anadir} />
+                                        </div>
                                     </Tooltip>
                                 ),
                                 value: 'agregar',
                             },
                         ]}
-                        value={selectedOficio}
                         onChange={handleOficioChange}
                     />
                 </Col>
                 <Col md={12}>
-                    {console.log('Estp es : '+selectedOficio)}
-                    <EditarEmpleado oficio={selectedOficio}/>
+                    <Select
+                        style={{ width: '100%' }}
+                        placeholder="Seleccione una sucursal"
+                        onChange={handleSucursalChange}
+                        loading={loadingSucursales}
+                    >
+                        {sucursales.map((sucursal) => (
+                            <Option key={sucursal.id_sucursal} value={sucursal.snombre}>
+                                {sucursal.snombre}
+                            </Option>
+                        ))}
+                    </Select>
+                </Col>
+                <Col md={12}>
+                    {console.log('Esto es : ' + selectedOficio)}
+                    <EditarEmpleado oficio={selectedOficio} sucursal={selectedSucursal} />
                 </Col>
             </Row>
-            <Drawer
-                title="Crear empleado"
-                width={720}
-                onClose={onClosee}
-                open={opene}
-                styles={{
-                    body: {
-                        paddingBottom: 80,
-                    },
-                }}
-            >
-                <CrearEmpleadoForm></CrearEmpleadoForm>
-            </Drawer>
         </>
     );
 };
