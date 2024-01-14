9bdc09a02ac68d7f0063e24c14703b8e003b8512
Jose Alvarez <jalvarezm7@uteq.edu.ec>
2024-01-14 02:56:05 -0500

    Empezando con producto

--- src/components/empleados.jsx
+++ src/components/empleados.jsx
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React,{useState} from 'react';
 import { UserOutlined } from '@ant-design/icons';
-<<<<<<< HEAD
 import { Divider, Avatar, Flex, Segmented, Tooltip,Drawer  } from 'antd';
-=======
-import { Divider, Avatar, Flex, Segmented, Tooltip, Select } from 'antd';
->>>>>>> ff473d4b513e55b35c3457006916c98fe7e88c2c
 import { Container, Row, Col } from 'react-bootstrap';
-import repartidor from './res/repartidor.png';
-import administrador from './res/administrador.png';
+import repartidor from './res/repartidor.png'
+import administrador from './res/administrador.png'
 import camarero from './res/camarero.png';
 import cocinero from './res/cocinero.png';
 import EditarEmpleado from './EditarEmpleado';
-<<<<<<< HEAD
 import anadir from './res/anadir.png'
-import CrearEmpleadoForm from './crearempleado';
 
 const Empleados = ({ }) => {
     const [selectedOficio, setSelectedOficio] = useState('Administradores');
@@ -27,31 +21,6 @@ const Empleados = ({ }) => {
         setSelectedOficio('Administradores');
         setOpene(false);
     };
-=======
-import anadir from './res/anadir.png';
-
-const { Option } = Select;
-
-const Empleados = ({ }) => {
-    const [selectedOficio, setSelectedOficio] = useState('Administradores');
-    const [selectedSucursal, setSelectedSucursal] = useState(0);
-    const [sucursales, setSucursales] = useState([]);
-    const [loadingSucursales, setLoadingSucursales] = useState(true);
-
-    useEffect(() => {
-        fetch('http://127.0.0.1:8000/sucursal/sucursaleslist/')
-            .then((response) => response.json())
-            .then((data) => {
-                setSucursales(data);
-            })
-            .catch((error) => {
-                console.error('Error fetching sucursales:', error);
-            })
-            .finally(() => {
-                setLoadingSucursales(false);
-            });
-    }, []);
->>>>>>> ff473d4b513e55b35c3457006916c98fe7e88c2c
 
     const handleOficioChange = (value) => {
         if (value == 'agregar') {
@@ -61,9 +30,6 @@ const Empleados = ({ }) => {
         setSelectedOficio(value);
     };
 
-    const handleSucursalChange = (value) => {
-        setSelectedSucursal(value);
-    };
 
     return (
         <>
@@ -98,7 +64,7 @@ const Empleados = ({ }) => {
                                 label: (
                                     <Tooltip title="Meseros">
                                         <div style={{ padding: 4 }}>
-                                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} size="large" src={camarero} />
+                                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} size="large" src={camarero}/>
                                         </div>
                                     </Tooltip>
                                 ),
@@ -108,8 +74,7 @@ const Empleados = ({ }) => {
                                 label: (
                                     <Tooltip title="Jefes de cocina">
                                         <div style={{ padding: 4 }}>
-                                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} size="large" src={cocinero} />
-                                        </div>
+                                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} size="large" src={cocinero}/>                                        </div>
                                     </Tooltip>
                                 ),
                                 value: 'JefesCocina',
@@ -118,8 +83,7 @@ const Empleados = ({ }) => {
                                 label: (
                                     <Tooltip title="Agregar empleados">
                                         <div style={{ padding: 4 }}>
-                                            <Avatar style={{ backgroundColor: '#ffff' }} icon={<UserOutlined />} size="large" src={anadir} />
-                                        </div>
+                                            <Avatar style={{ backgroundColor: '#ffff' }} icon={<UserOutlined />} size="large" src={anadir}/>                                        </div>
                                     </Tooltip>
                                 ),
                                 value: 'agregar',
@@ -129,22 +93,8 @@ const Empleados = ({ }) => {
                     />
                 </Col>
                 <Col md={12}>
-                    <Select
-                        style={{ width: '100%' }}
-                        placeholder="Seleccione una sucursal"
-                        onChange={handleSucursalChange}
-                        loading={loadingSucursales}
-                    >
-                        {sucursales.map((sucursal) => (
-                            <Option key={sucursal.id_sucursal} value={sucursal.snombre}>
-                                {sucursal.snombre}
-                            </Option>
-                        ))}
-                    </Select>
-                </Col>
-                <Col md={12}>
-                    {console.log('Esto es : ' + selectedOficio)}
-                    <EditarEmpleado oficio={selectedOficio} sucursal={selectedSucursal} />
+                    {console.log('Estp es : '+selectedOficio)}
+                    <EditarEmpleado oficio={selectedOficio}/>
                 </Col>
             </Row>
             <Drawer
